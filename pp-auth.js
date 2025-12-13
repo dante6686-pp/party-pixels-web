d75789aa561a299af0d07c392e189498e41dc1c1
dante6686-pp <dante6686@gmail.com>
2025-12-13 06:28:21 +0100

    Update pp-auth.js

--- pp-auth.js
+++ pp-auth.js
@@ -1,5 +1,8 @@
 // pp-auth.js
 
+const PP_LOGIN_URL = "/account.html";
+const PP_PROFILE_URL = "/profile.html";
+
 // 1) Supabase client
 const SUPABASE_URL = "https://dyfrzwxhycqnqntvkuxy.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5ZnJ6d3hoeWNxbnFudHZrdXh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODM5MjgsImV4cCI6MjA4MDE1OTkyOH0.n4jP0q7YZY-jQaSnHUkKWyI9wM02iHXnRXS31AATnY0";
